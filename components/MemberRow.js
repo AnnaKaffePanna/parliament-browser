ed4b87a96ea2cb2ba089ee410e72565d47ed0347
Anna Ö <anna.ostman24@gmail.com>
2024-04-09 12:58:35 +0300

    Update memberRow layout and add asset for no image

--- components/MemberRow.js
+++ components/MemberRow.js
@@ -1,9 +1,43 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import noImage from "../assets/no-image.png";
 
-export const MembersRow = ({ member }) => {
+export const MemberRow = ({ member }) => {
   return (
-    <View>
-      <Text>{member.name}</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        {member.image && member.image.url ? (
+          <Image source={{ uri: member.image.url }} style={styles.image} />
+        ) : (
+          <Image source={noImage} style={styles.image} />
+        )}
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>{member.name}</Text>
+        <Text style={styles.text}>{member.city}</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+  },
+  imageContainer: {
+    marginRight: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: "contain",
+  },
+  textContainer: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 16,
+  },
+});
